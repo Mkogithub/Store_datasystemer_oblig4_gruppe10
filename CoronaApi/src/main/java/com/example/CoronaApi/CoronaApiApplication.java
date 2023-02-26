a5203d0e63c0294b90e58b6e099a6b4c1d5a9bed
mko095 <mko095@post.uit.no>
2023-02-26 16:27:49 +0100

    Merged with dev 2 so that main branch is updated

--- CoronaApi/src/main/java/com/example/CoronaApi/CoronaApiApplication.java
+++ CoronaApi/src/main/java/com/example/CoronaApi/CoronaApiApplication.java
@@ -27,7 +27,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
 @SpringBootApplication
 @EnableSwagger2
 public class CoronaApiApplication {
